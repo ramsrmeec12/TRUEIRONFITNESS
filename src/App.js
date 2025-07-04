6b10c02dd55a6a0593db87773cf08f703452aae7
ramsrmeec12 <ramskv1018@gmail.com>
2025-07-04 23:50:27 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import ViewClients from "./pages/ViewClients";
 import AddFoodItem from "./pages/AddFoodItem";
 import AddWorkout from "./pages/AddWorkout";
 import ClientProfile from "./pages/ClientProfile";
+import Home from "./pages/Home";
+import TrainerLogin from "./pages/TrainerLogin";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/add-client" element={<AddClient />} />
         <Route path="/view-clients" element={<ViewClients />} />
         <Route path="/add-food" element={<AddFoodItem />} />
@@ -21,7 +24,7 @@ function App() {
         <Route path="/client-login" element={<ClientLogin />} />
         <Route path="/client-dashboard" element={<ClientDashboard />} />
         <Route path="/client/:id" element={<ClientProfile />} />
-
+        <Route path="/trainer-login" element={<TrainerLogin></TrainerLogin>}></Route>
       </Routes>
     </BrowserRouter>
   );
